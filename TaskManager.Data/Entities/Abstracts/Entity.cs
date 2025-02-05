c7519b0eec811d58ba85011dba7af4276e29e31d
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-05 21:37:18 +0200

    add abstract entities

---
+++ TaskManager.Data/Entities/Abstracts/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Data.Entities.Abstracts
+{
+    public abstract class Entity
+    {
+        public Guid ID { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ TaskManager.Data/Entities/Abstracts/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Data.Helpers;
+
+namespace TaskManager.Data.Entities.Abstracts
+{
+    public abstract class Person : Entity
+    {
+        [MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+        [MaxLength(256)]
+        public string Password { get; set; } = string.Empty;
+        [MaxLength(20)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(20)]
+        public string LastName { get; set; } = string.Empty;
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
