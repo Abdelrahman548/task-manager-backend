d22d90fcd1d9eb495b4844f9ce0b47949d9ac544
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-05 23:42:21 +0200

    add base repo interfaces

---
+++ TaskManager.Repository/Interfaces/Base/ICreateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Repository.Interfaces.Base
+{
+    public interface ICreateRepository<T>
+        where T : class
+    {
+        Task AddAsync(T item);
+    }
+}
---
+++ TaskManager.Repository/Interfaces/Base/IReadRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Repository.Interfaces.Base
+{
+    public interface IReadRepository<T>
+        where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
