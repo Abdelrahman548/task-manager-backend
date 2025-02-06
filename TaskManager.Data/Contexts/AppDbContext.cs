180580bd4dd3f530f03c137820adc547ad06f4c3
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-06 14:39:25 +0200

    ignore searchable property for EF core

--- TaskManager.Data/Contexts/AppDbContext.cs
+++ TaskManager.Data/Contexts/AppDbContext.cs
@@ -5,12 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManager.Data.Entities;
+using TaskManager.Data.Entities.Abstracts;
 
 namespace TaskManager.Data.Contexts
 {
     public class AppDbContext: DbContext
     {
         public AppDbContext(DbContextOptions options): base(options){}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entity>().Ignore(e => e.SearchableProperty);
+            base.OnModelCreating(modelBuilder);
+        }
         public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<Manager> Managers { get; set; }
         public virtual DbSet<Department> Departments { get; set; }
