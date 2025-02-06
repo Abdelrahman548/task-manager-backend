ffb279b032b936c67411b5722fb320d977bc5612
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-06 22:22:10 +0200

    add controllers

---
+++ TaskManager.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+    }
+}
