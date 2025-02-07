9b8b6e0cbcfba5790b60d0e90feee1f0a1a83ce9
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-07 17:08:59 +0200

    implement manager controller

--- TaskManager.Web/Controllers/ManagersController.cs
+++ TaskManager.Web/Controllers/ManagersController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Repository.Helpers;
+using TaskManager.Service.DTOs.Request;
+using TaskManager.Service.DTOs.Response;
+using TaskManager.Service.Helpers;
+using TaskManager.Service.Implementations;
+using TaskManager.Service.Interfaces;
 
 namespace TaskManager.Web.Controllers
 {
@@ -6,5 +12,41 @@ namespace TaskManager.Web.Controllers
     [Route("[controller]")]
     public class ManagersController : ControllerBase
     {
+        private readonly IManagerService managerService;
+
+        public ManagersController(IManagerService managerService)
+        {
+            this.managerService = managerService;
+        }
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<BaseResult<ManagerResponseDto>>> Get(Guid id)
+        {
+            var result = await managerService.Get(id);
+
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<BaseResult<PagedList<ManagerResponseDto>>>> GetAll([FromQuery] ItemQueryParameters queryParameters)
+        {
+            var result = await managerService.Get(queryParameters);
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<BaseResult<Guid>>> Update(Guid id, ManagerRequestDto dto)
+        {
+            var result = await managerService.Update(dto, id);
+
+            return StatusCode((int)result.StatusCode, result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<BaseResult<string>>> Delete(Guid id)
+        {
+            var result = await managerService.Delete(id);
+
+            return StatusCode((int)result.StatusCode, result);
+        }
     }
 }
