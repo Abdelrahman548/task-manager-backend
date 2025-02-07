857cd745b4b647827fec4ee6d4f3fc7ae0a44817
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-07 16:28:42 +0200

    add exception handling middlware

---
+++ TaskManager.Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using TaskManager.Service.Helpers;
+using System.Text.Json;
+
+namespace TaskManager.Web.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ExceptionHandlingMiddleware> logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }catch(Exception e)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var responseModel = new BaseResult<string>() { IsSuccess = false, Errors = [e?.Message], StatusCode = MyStatusCode.InternalServerError };
+                response.StatusCode = (int)MyStatusCode.InternalServerError;
+                var result = JsonSerializer.Serialize(responseModel);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
