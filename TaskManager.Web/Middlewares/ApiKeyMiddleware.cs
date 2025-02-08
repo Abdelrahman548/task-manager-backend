bcd3a2f655c22231f45b3db9e2fd427fb5639316
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-08 19:21:15 +0200

    add and register API key middleware

---
+++ TaskManager.Web/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using TaskManager.Service.Helpers;
+
+namespace TaskManager.Web.Middlewares
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<ApiKeyMiddleware> logger;
+        private readonly IConfiguration configuration;
+        private const string API_KEY_HEADER = "Api-Key";
+
+
+        public ApiKeyMiddleware(RequestDelegate next, ILogger<ApiKeyMiddleware> logger, IConfiguration configuration)
+        {
+            this.next = next;
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(API_KEY_HEADER, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                var result = new BaseResult<string>() { IsSuccess = false, StatusCode = MyStatusCode.Unauthorized, Errors = ["API KEY is missing"] };
+                var serilaizedResult = JsonSerializer.Serialize(result);
+                await context.Response.WriteAsync(serilaizedResult);
+                return;
+            }
+            if(CheckApiKey(extractedApiKey))
+            {
+                await next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 403;
+                var result = new BaseResult<string>() { IsSuccess = false, StatusCode = MyStatusCode.Forbidden , Errors = ["Invalid API KEY"] };
+                var serilaizedResult = JsonSerializer.Serialize(result);
+                await context.Response.WriteAsync(serilaizedResult);
+                return;
+            }
+        }
+        private bool CheckApiKey(string? key)
+        {
+            var MyKey = configuration["APIKEY"];
+            return MyKey?.Equals(key) ?? false;
+        }
+    }
+}
--- TaskManager.Web/Program.cs
+++ TaskManager.Web/Program.cs
@@ -108,6 +108,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseHttpsRedirection();
 
