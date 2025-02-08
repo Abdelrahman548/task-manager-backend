6080130fe2c0ebb9b3f6924db7ea47bb80fe45d7
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-08 18:47:10 +0200

    apply RE on Passwords

--- TaskManager.Service/DTOs/SignIn/ManagerSignInDto.cs
+++ TaskManager.Service/DTOs/SignIn/ManagerSignInDto.cs
@@ -16,6 +16,7 @@ namespace TaskManager.Service.DTOs.SignIn
         public string Username { get; set; } = string.Empty;
         [Required]
         [MaxLength(50), MinLength(10)]
+        [RegularExpression("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$!%*?&#^(){}[\\]<>_+=|\\\\~`:;,.\\/-])[A-Za-z\\d@$!%*?&#^(){}[\\]<>_+=|\\\\~`:;,.\\/-]{10,}$")]
         public string Password { get; set; } = string.Empty;
     }
 }
