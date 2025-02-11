f0c5362abb01a053988f4cd0d4ddbedab140848c
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-11 20:38:14 +0200

    add otp generator

---
+++ TaskManager.Service/Helpers/OTPGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Service.Helpers
+{
+    public static class OTPGenerator
+    {
+        public static string GenerateOTP(int length = 6)
+        {
+            var random = new Random();
+            return random.Next((int)Math.Pow(10, length - 1), (int)Math.Pow(10, length)).ToString();
+        }
+    }
+}
