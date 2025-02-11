caa14e29cce27b77f83d6673a5115ae1d9a57558
Abdelrahman Amr <abdelrahmanamr68@gmail.com>
2025-02-11 20:54:23 +0200

    add SmtpServer to email options

--- TaskManager.Service/Helpers/EmailOptions.cs
+++ TaskManager.Service/Helpers/EmailOptions.cs
@@ -8,6 +8,7 @@ namespace TaskManager.Service.Helpers
 {
     public class EmailOptions
     {
+        public string SmtpServer { get; set; }
         public string SenderEmail { get; set; }
         public string SenderName { get; set; }
         public string AppPassword { get; set; }
